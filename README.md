6054fb28e935a1b22e1b404b628d85ad8643ff92
Josue <josuecarlosos2@gmail.com>
2022-05-06 02:32:35 -0300

    lifcicle

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Life Cycle - Ciclo de Vida do Componente
 
+- O que são [Hooks](https://www.devmedia.com.br/o-que-sao-lifecycle-hooks/40717)? hook significa gancho,  em síntese é um conjunto de métodos que são invocados no momento que houve algum evento de criação/modificação do componente.
 
+
+
+> - **[`ngOnInit`]**()
+> - **[`ngOnChanges`]**()  
+> - **[`ngAfterContentInit`]**()  
+> - **[`ngAfterViewInit`]**()  
+> - **[`ngOnDestroy`]**()  
+> - **[`ngOnChanges`]**()  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { ChildChildComponent } from './main-lifecycle/lifecycle-child/child-child/child-child.component';
+import { LifecycleChildComponent } from './main-lifecycle/lifecycle-child/lifecycle-child.component';
+import { MainLifecycleComponent } from './main-lifecycle/main-lifecycle.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    ChildChildComponent,
+    MainLifecycleComponent,
+    AppComponent,
+    LifecycleChildComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
